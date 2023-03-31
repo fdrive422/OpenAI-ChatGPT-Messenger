300501246ca786ccab0699826054f8a7bd4aa3a8
fdrive422 <fafrago@gmail.com>
2023-03-31 16:11:42 -0700

    update modelseleciton

--- components/ModelSelection.tsx
+++ components/ModelSelection.tsx
@@ -1,37 +1,40 @@
-import React from 'react';
-import useSWR from 'swr'
-import Select from 'react-select'
+import React from "react";
+import useSWR from "swr";
+import Select from "react-select";
 
 type Props = {};
 
-const fetchModels = () => fetch('/api/get-engines').then(res => res.json())
+const fetchModels = () => fetch("/api/get-engines").then((res) => res.json());
 
-const ModelSelection: React.FC<Props> = ({ }) => {
-    // use to fecth data
-    const { data, error, isLoading } = useSWR('listModels', fetchModels)
+const ModelSelection: React.FC<Props> = ({}) => {
+	// use to fetch data
+	const { data, error, isLoading } = useSWR("listModels", fetchModels);
 
-    // use to update data like useState
-    const { data: model, mutate: setModel } = useSWR('selectedModel', {
-        fallbackData: 'text-davinci-003'
-    })
+	// use to update data like useState
+	const { data: model, mutate: setModel } = useSWR("selectedModel", {
+		fallbackData: "text-davinci-003",
+	});
 
-    return (
-        <div className='mt-2'>
-            <Select
-                className='mt-2'
-                options={data?.modelOptions}
-                defaultValue={model}
-                placeholder={model}
-                isClearable
-                isLoading={isLoading}
-                menuPosition='fixed'
-                classNames={{
-                    control: (state) => 'text-white bg-[#434654] border-[#434654]'
-                }}
-                onChange={(e?: { value: string, label: string }) => setModel(e?.value)}
-            />
-        </div>
-    );
-}
+	return (
+		<div className="mt-2">
+			<Select
+				className="mt-2"
+				options={data?.modelOptions}
+				defaultValue={model}
+				placeholder={model}
+				isClearable
+				isLoading={isLoading}
+				menuPosition="fixed"
+				classNames={{
+					control: (state) =>
+						"text-white bg-[#434654] border-[#434654]",
+				}}
+				onChange={(e?: { value: string; label: string }) =>
+					setModel(e?.value)
+				}
+			/>
+		</div>
+	);
+};
 
 export default ModelSelection;
