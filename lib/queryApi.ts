29fb031271236bd2ef4311dbb0f29bdaaa8c6ebe
fdrive422 <fafrago@gmail.com>
2023-04-09 21:48:35 -0700

    update queryApi function settings

--- lib/queryApi.ts
+++ lib/queryApi.ts
@@ -1,49 +1,46 @@
 import openai from "./chatgpt"
 
-const query = async (text: string, chatId: string, model: string) => {
-    const res = openai.createCompletion({
-        model,
-        prompt: text,
-        temperature: 0, //0.9 0.7 0
-        top_p: 1,
-        max_tokens: 4000, //1000 64 3000
-        frequency_penalty: 0.5, // 0 0.5
-        presence_penalty: 0
-    })
-        .then((res) => {
-            // console.log(res.data.choices)
-            // console.log(res.data.choices.length)
-            return res.data.choices[0].text
+// const query = async (text: string, chatId: string, model: string) => {
+//     const res = openai.createCompletion({
+//         model,
+//         prompt: text,
+//         temperature: 0, //0.9 0.7 0
+//         top_p: 1,
+//         max_tokens: 4000, //1000 64 3000
+//         frequency_penalty: 0.5, // 0 0.5
+//         presence_penalty: 0
+//     })
+//         .then((res) => {
+//             // console.log(res.data.choices)
+//             // console.log(res.data.choices.length)
+//             return res.data.choices[0].text
+//         })
+//         // .then((res) => res.data.choices)
+//         .catch(err => {
+//             console.log(err)
+//             return `ChatGPT was unable to find an answer for that! (${err})`
+//         })
+
+//     return res
+
+const query = async (prompt: string, chatId: string, model: string) => {
+    const res = await openai
+        .createCompletion({
+            model,
+            prompt,
+            temperature: 0.9,
+            top_p: 1,
+            max_tokens: 1000,
+            frequency_penalty: 0,
+            presence_penalty: 0,
         })
-        // .then((res) => res.data.choices)
-        .catch(err => {
-            console.log(err)
-            return `ChatGPT was unable to find an answer for that! (${err})`
-        })
+        .then((res) => res.data.choices[0].text)
+        .catch(
+            (err) =>
+                `ChatPGT was unable to find an answer for that! (Error : ${err.message})`
+        );
 
-    return res
-
-    // const res = openai.createModeration({
-    //     model: 'text-moderation-playground',
-    //     input: text,
-    //     // temperature: 0.9,
-    //     // top_p: 1,
-    //     // max_tokens: 1000,
-    //     // frequency_penalty: 0,
-    //     // presence_penalty: 0
-    // })
-    //     .then((res) => {
-    //         // console.log(res.data.choices)
-    //         console.log(res.data.results)
-    //         return 'Success'
-    //     })
-    //     // .then((res) => res.data.choices)
-    //     .catch(err => {
-    //         console.log(err)
-    //         return `ChatGPT was unable to find an answer for that! (Error: ${err})`
-    //     })
-
-    // return res
+    return res;
 }
 
 export default query
