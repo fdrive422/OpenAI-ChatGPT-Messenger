da0bc4726fccb3e8ef622ab2a031fc8bde757f85
fdrive422 <fafrago@gmail.com>
2023-04-14 19:30:33 -0700

    clean up comments

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1,104 +1,118 @@
-'use client'
+"use client";
 
-import { PaperAirplaneIcon } from '@heroicons/react/24/outline';
-import { addDoc, collection, serverTimestamp, Timestamp } from 'firebase/firestore';
-import { useSession } from 'next-auth/react';
-import React, { FormEvent, useState } from 'react';
-import { db } from '../utils/firebase';
-import { toast } from 'react-hot-toast'
-import useSWR from 'swr'
-import ModelSelection from './ModelSelection';
+import { PaperAirplaneIcon } from "@heroicons/react/24/outline";
+import {
+	addDoc,
+	collection,
+	serverTimestamp,
+	Timestamp,
+} from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import React, { FormEvent, useState } from "react";
+import { db } from "../utils/firebase";
+import { toast } from "react-hot-toast";
+import useSWR from "swr";
+import ModelSelection from "./ModelSelection";
 
 type Props = {
-    chatId: string
+	chatId: string;
 };
 
 const ChatInput: React.FC<Props> = ({ chatId }) => {
-    const [prompt, setPrompt] = useState('')
-    const { data: session } = useSession()
+	const [prompt, setPrompt] = useState("");
+	const { data: session } = useSession();
 
-    // TODO: useSWR to get model
-    // const model = 'text-davinci-003'
-    // text-moderation-playground
-    // text-davinci-002-render-sha
-    const { data: model } = useSWR('selectedModel', {
-        fallbackData: 'text-davinci-003'
-    })
+	const { data: model } = useSWR("selectedModel", {
+		fallbackData: "text-davinci-003",
+	});
 
-    const sendMessage = async (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        if (!prompt) return
+	const sendMessage = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		if (!prompt) return;
 
+		// TOAST NOTIFICATION
+		const notification = toast.loading("ChatGPT is thinking...");
 
-        // TOAST NOTIFICATION
-        const notification = toast.loading('ChatGPT is thinking...')
+		const input = prompt.trim();
+		setPrompt("");
 
+		const message: Message = {
+			text: input,
+			// createdAt: serverTimestamp(),
+			createdAt: Timestamp.now(),
+			user: {
+				_id: session?.user?.email!,
+				name: session?.user?.name!,
+				avatar:
+					session?.user?.image ||
+					`https://ui-avatars.com/api?name=${session?.user?.name}`,
+			},
+		};
 
-        const input = prompt.trim()
-        setPrompt('')
+		// ADD ROW MESSAGE TO FIREBASE
+		await addDoc(
+			collection(
+				db,
+				"users",
+				session?.user?.email!,
+				"chats",
+				chatId,
+				"messages"
+			),
+			message
+		);
 
-        const message: Message = {
-            text: input,
-            // createdAt: serverTimestamp(),
-            createdAt: Timestamp.now(),
-            user: {
-                _id: session?.user?.email!,
-                name: session?.user?.name!,
-                avatar: session?.user?.image || `https://ui-avatars.com/api?name=${session?.user?.name}`
-            }
-        }
+		// ASK QUESTION TO OPENAI
+		await fetch("/api/ask-question", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				text: input,
+				chatId,
+				model,
+				session,
+				timestamp: Timestamp.now(),
+			}),
+		})
+			.then(() => {
+				// TOAST NOTIFICATION TO SAY SUCCESS
+				toast.success("ChatGPT has responded!", {
+					id: notification,
+				});
+			})
+			.catch((err) => {
+				toast.error(`Backend (Error: ${err.message})`, {
+					id: notification,
+				});
+			});
+	};
 
-        // ADD ROW MESSAGE TO FIREBASE
-        await addDoc(
-            collection(db, 'users', session?.user?.email!, 'chats', chatId, 'messages'),
-            message
-        )
+	return (
+		<div className="bg-white text-gray-800 dark:bg-gray-700/50 dark:text-gray-400 rounded-lg text-sm border-t-2 border-gray-300 dark:border-transparent dark:border-none">
+			<form onSubmit={sendMessage} className="p-5 space-x-5 flex">
+				<input
+					className="bg-transparent focus:outline-none flex-1 disabled:cursor-not-allowed disabled:text-gray-300"
+					disabled={!session}
+					value={prompt}
+					onChange={(e) => setPrompt(e.target.value)}
+					placeholder="Type your message here..."
+				/>
+				<button
+					type="submit"
+					disabled={!prompt || !session}
+					className="bg-[#11A37F] hover:opacity-50 text-white font-semibold px-4 py-2 rounded cursor-pointer duration-300 disabled:bg-gray-300 disabled:cursor-not-allowed">
+					<PaperAirplaneIcon className="h-4 w-4 -rotate-45" />
+				</button>
+			</form>
 
-        // ASK QUESTION TO OPENAI
-        await fetch('/api/ask-question', {
-            method: 'POST',
-            headers: {
-                "Content-Type": 'application/json'
-            },
-            body: JSON.stringify({
-                text: input,
-                chatId,
-                model,
-                session,
-                timestamp: Timestamp.now()
-            })
-        }).then(() => {
-            // TOAST NOTIFICATION TO SAY SUCCESS
-            toast.success('ChatGPT has responded!', {
-                id: notification
-            })
-        }).catch((err) => {
-            toast.error(`Backend (Error: ${err.message})`, {
-                id: notification
-            })
-        })
-    }
-
-    return (
-        <div className='bg-white text-gray-800 dark:bg-gray-700/50 dark:text-gray-400 rounded-lg text-sm border-t-2 border-gray-300 dark:border-transparent dark:border-none'>
-            <form onSubmit={sendMessage} className='p-5 space-x-5 flex'>
-                <input className='bg-transparent focus:outline-none flex-1 disabled:cursor-not-allowed disabled:text-gray-300'
-                    disabled={!session}
-                    value={prompt}
-                    onChange={e => setPrompt(e.target.value)}
-                    placeholder='Type your message here...'
-                />
-                <button type='submit' disabled={!prompt || !session} className='bg-[#11A37F] hover:opacity-50 text-white font-semibold px-4 py-2 rounded cursor-pointer duration-300 disabled:bg-gray-300 disabled:cursor-not-allowed'>
-                    <PaperAirplaneIcon className='h-4 w-4 -rotate-45' />
-                </button>
-            </form>
-
-            <div className='sm:hidden'>
-                {/* MODEL SELECTION */}
-                <ModelSelection />
-            </div>
-        </div>
-    );
-}
+			<div className="sm:hidden">
+				{/* MODEL SELECTION */}
+				<ModelSelection />
+			</div>
+		</div>
+	);
+};
 
 export default ChatInput;
--- components/NewChat.tsx
+++ components/NewChat.tsx
@@ -1,42 +1,44 @@
-import React, { MouseEventHandler } from 'react';
-import { useRouter } from 'next/navigation';
-import { PlusIcon } from '@heroicons/react/24/outline';
-import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
-import { useSession } from 'next-auth/react';
-import { db } from '../utils/firebase';
+import React, { MouseEventHandler } from "react";
+import { useRouter } from "next/navigation";
+import { PlusIcon } from "@heroicons/react/24/outline";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import { db } from "../utils/firebase";
 
 type Props = {
-    onNewChat: (e: React.MouseEventHandler<HTMLButtonElement | HTMLButtonElement>) => any
+	onNewChat: (
+		e: React.MouseEventHandler<HTMLButtonElement | HTMLButtonElement>
+	) => any;
 };
 
-
-// c_user {email} have => chats {message, userId, createdAt}
 const NewChat: React.FC<Props> = ({ onNewChat }) => {
-    const { data: session } = useSession()
-    const router = useRouter()
+	const { data: session } = useSession();
+	const router = useRouter();
 
-    const createNewChat = async (e: any) => {
-        const doc = await addDoc(
-            collection(db, 'users', session?.user?.email!, 'chats'), {
-            // messages: [],
-            userId: session?.user?.email!,
-            createdAt: serverTimestamp()
-        }
-        )
-        /* users > user@gmail.com > chats > VnEX06OtWiJe0AHt9h5H > {message: [], userId: user@gmail.com, createdAt: 13 February 2023 at 19:12:49 UTC+7} */
+	const createNewChat = async (e: any) => {
+		const doc = await addDoc(
+			collection(db, "users", session?.user?.email!, "chats"),
+			{
+				// messages: [],
+				userId: session?.user?.email!,
+				createdAt: serverTimestamp(),
+			}
+		);
 
-        onNewChat(e)
-        router.push(`/chat/${doc.id}`)
-    }
+		onNewChat(e);
+		router.push(`/chat/${doc.id}`);
+	};
 
-    return (
-        <>
-            <div onClick={createNewChat} className='border-gray-700 border chatRow'>
-                <PlusIcon className='h-4 w-4' />
-                <p>New Chat</p>
-            </div>
-        </>
-    );
-}
+	return (
+		<>
+			<div
+				onClick={createNewChat}
+				className="border-gray-700 border chatRow">
+				<PlusIcon className="h-4 w-4" />
+				<p>New Chat</p>
+			</div>
+		</>
+	);
+};
 
 export default NewChat;
